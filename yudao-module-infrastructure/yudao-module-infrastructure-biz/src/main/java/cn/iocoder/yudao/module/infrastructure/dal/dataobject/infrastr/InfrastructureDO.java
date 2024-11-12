e8adc6ec07504f8a781470e0815aa2c89ce2a58a
LAPTOP-04UDB8NI\97042 <970422076@qq.com>
2024-11-12 12:43:12 +0800

    第一张表的主键自增

--- yudao-module-infrastructure/yudao-module-infrastructure-biz/src/main/java/cn/iocoder/yudao/module/infrastructure/dal/dataobject/infrastr/InfrastructureDO.java
+++ yudao-module-infrastructure/yudao-module-infrastructure-biz/src/main/java/cn/iocoder/yudao/module/infrastructure/dal/dataobject/infrastr/InfrastructureDO.java
@@ -17,7 +17,7 @@ import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
  * @author Bagration C
  */
 @TableName("infrastructure")
-@KeySequence("infrastructure_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
+@KeySequence("infrastructure_id_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = false)
 @ToString(callSuper = true)
