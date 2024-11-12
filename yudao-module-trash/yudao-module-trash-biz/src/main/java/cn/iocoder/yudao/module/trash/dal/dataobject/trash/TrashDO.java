7fa7aa709f6c63e0194971c89971e91604a3216d
LAPTOP-04UDB8NI\97042 <970422076@qq.com>
2024-11-12 15:10:48 +0800

    修复新增bug（主键自增约束名称）

--- yudao-module-trash/yudao-module-trash-biz/src/main/java/cn/iocoder/yudao/module/trash/dal/dataobject/trash/TrashDO.java
+++ yudao-module-trash/yudao-module-trash-biz/src/main/java/cn/iocoder/yudao/module/trash/dal/dataobject/trash/TrashDO.java
@@ -12,7 +12,7 @@ import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
  * @author Bagration C
  */
 @TableName("trash_can")
-@KeySequence("trash_can_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
+@KeySequence("trash_can_extension_id_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
